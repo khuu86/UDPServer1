641b20a0d4c27d03676cd2a22df05e6c7de92b93
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-09-12 11:13:41 +0200

    Add project files.

---
+++ UDPServer1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("UDP Server");
+
+using (UdpClient socket = new UdpClient())
+{
+    socket.Client.Bind(new IPEndPoint(IPAddress.Any, 5005));
+
+    IPEndPoint clientEndpoint = null;
+    while (true)
+    {
+        byte[] received = socket.Receive(ref clientEndpoint);
+
+        string stringResult = Encoding.UTF8.GetString(received);
+
+        Console.WriteLine(stringResult);
+    }
+}
