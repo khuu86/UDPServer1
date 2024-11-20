fc753c69973c0568080f00ce59dff17eb8caba23
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-11-20 06:19:20 +0100

    Tilføjet kommentarer og forklaring

--- UDPServer1/Program.cs
+++ UDPServer1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Se https://aka.ms/new-console-template for mere information
 
 using System.Net;
 using System.Net.Sockets;
@@ -6,17 +6,17 @@ using System.Text;
 
 Console.WriteLine("UDP Server");
 
-using (UdpClient socket = new UdpClient())
+using (UdpClient socket = new UdpClient()) // Opret en ny UdpClient instans
 {
-    socket.Client.Bind(new IPEndPoint(IPAddress.Any, 5005));
+    socket.Client.Bind(new IPEndPoint(IPAddress.Any, 5005)); // Bind socketen til enhver IP-adresse og port 5005
 
-    IPEndPoint clientEndpoint = null;
-    while (true)
+    IPEndPoint clientEndpoint = null; // Initialiser en variabel til at gemme klientens endpoint
+    while (true) // Uendelig løkke for at holde serveren kørende
     {
-        byte[] received = socket.Receive(ref clientEndpoint);
+        byte[] received = socket.Receive(ref clientEndpoint); // Modtag data fra en klient
 
-        string stringResult = Encoding.UTF8.GetString(received);
+        string stringResult = Encoding.UTF8.GetString(received); // Konverter de modtagne bytes til en streng
 
-        Console.WriteLine(stringResult);
+        Console.WriteLine(stringResult); // Udskriv den modtagne besked til konsollen
     }
 }
